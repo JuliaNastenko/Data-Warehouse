1236a5b191ba9f895b7754567c52b86d1c2c9d98
JuliaNastenko <juli.nastenko@gmail.com>
2020-11-09 23:04:21 +0200

    initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const burger = document.getElementById('burger-menu');
+const menu = document.getElementById('mob-menu');
+const main = document.getElementById('main-section');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active-burger');
+  menu.classList.toggle('active-menu');
+  main.classList.toggle('is-blured');
+});
+
+const btn = document.querySelectorAll('.btn');
+const popup = document.getElementById('popup');
+const content = document.getElementById('content');
+
+btn.forEach(element => {
+  element.addEventListener('click', () => {
+    popup.classList.add('open');
+    content.classList.add('content-visible');
+  });
+});
+
+popup.addEventListener('click', (event) => {
+  if (event.target.classList.contains('popup')) {
+    popup.classList.remove('open');
+    content.classList.remove('content-visible');
+  }
+});
